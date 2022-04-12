6b9d1fb241fe6717d1b055d53da2a2d765c48e9a
Fakepng <krit0kasemtewin@gmail.com>
2022-04-12 16:35:26 +0700

    FibonacciRecursion

---
+++ FibonacciRecursion/FibonacciRecursion.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func FibonacciRecursion(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return FibonacciRecursion(n-1) + FibonacciRecursion(n-2)
+}
+func main() {
+	fmt.Print("Enter the number of terms: ")
+	var terms int
+	fmt.Scanln(&terms)
+
+	for i := 0; i < terms; i++ {
+		fmt.Print(strconv.Itoa(FibonacciRecursion(i)) + " ")
+	}
+	fmt.Print("\n")
+	fmt.Print("Press any key to exit")
+	var exit string
+	fmt.Scanln(&exit)
+}
